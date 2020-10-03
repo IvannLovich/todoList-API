e08a6d37f23a1d211f5bd8416bc632ce8839701f
Iván Lovich <ivan.n.lovich@gmail.com>
2020-10-03 19:00:08 -0300

    endpoints test added

--- todo/tests.py
+++ todo/tests.py
@@ -1,3 +1,15 @@
 from django.test import TestCase
+from django.test import Client
+from .models import Folder
 
-# Create your tests here.
+client = Client()
+
+class FolderTestCase(TestCase):
+
+    def test_folder_endpoint(self):
+        response = client.get('/api/todo/folders/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_task_endpoint(self):
+        response = client.get('/api/todo/tasks/')
+        self.assertEqual(response.status_code, 200)
