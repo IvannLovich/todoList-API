4d93e9f15a34bfc81cbc902cded3e49bad8c8767
Iván Lovich <ivan.n.lovich@gmail.com>
2020-10-03 19:17:16 -0300

    response type and status tested

--- todo/tests.py
+++ todo/tests.py
@@ -4,12 +4,14 @@ from .models import Folder
 
 client = Client()
 
-class FolderTestCase(TestCase):
+class EndpointsTestCase(TestCase):
 
     def test_folder_endpoint(self):
         response = client.get('/api/todo/folders/')
         self.assertEqual(response.status_code, 200)
+        self.assertEqual(response['content-type'], 'application/json')
 
     def test_task_endpoint(self):
         response = client.get('/api/todo/tasks/')
         self.assertEqual(response.status_code, 200)
+        self.assertEqual(response['content-type'], 'application/json')
