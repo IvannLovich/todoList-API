8743e0d1cdbcd715fa32088dd73f619b08451718
Iván Lovich <ivan.n.lovich@gmail.com>
2020-10-03 23:05:42 -0300

    Update wsgi.py

--- abm/wsgi.py
+++ abm/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'abm.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'abm.settings.dev')
 
 application = get_wsgi_application()
